a4f37eb0c13eb10f0707ef5a238f84aabe17611c
Asim-A <asimabazi@hotmail.no>
2020-11-02 17:21:00 +0100

    Updates

---
+++ precache-manifest.371f93d7837902ac32d4d1e72f45d57c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4548a78e914e598ab9db4611db1ebe1",
+    "url": "/norkart-kurs/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart-kurs/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "eb042d9edf1356f37cba",
+    "url": "/norkart-kurs/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/norkart-kurs/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/norkart-kurs/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb042d9edf1356f37cba",
+    "url": "/norkart-kurs/static/js/main.9f14d492.chunk.js"
+  },
+  {
+    "revision": "0a19e80fe71ad8fa26a4",
+    "url": "/norkart-kurs/static/js/runtime-main.4d6094a0.js"
+  }
+]);
